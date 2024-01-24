8e426ff33c07b9a3c61519243433ced9cc8f38d5
Surendra Pandar <71205618+surendrachoudhary@users.noreply.github.com>
2024-01-24 11:21:43 +0530

    Time: 26 ms (99.93%) | Memory: 17.4 MB (90.10%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
@@ -6,36 +6,42 @@
 
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        # Create a dummy node to simplify handling the first group
         dummy = ListNode(0, head)
+        # Initialize groupPrev to the dummy node
         groupPrev = dummy
 
+        # Loop until there are no more groups of size k
         while True:
-            kth = self.getKth(groupPrev,k)
+            # Get the kth node in the current group
+            kth = self.getKth(groupPrev, k)
 
+            # If there are fewer than k nodes left, break the loop
             if not kth:
                 break
             
+            # Save the next node after the current group
             groupNext = kth.next 
 
-            #reverse group
-
+            # Reverse the current group
             prev, curr = kth.next, groupPrev.next
-
             while curr != groupNext:
                 tmp = curr.next
                 curr.next = prev
                 prev = curr
                 curr = tmp
                 
+            # Connect the reversed group to the previous group
             tmp = groupPrev.next 
             groupPrev.next = kth 
             groupPrev = tmp 
 
+        # Return the modified linked list
         return dummy.next
 
-
+    # Helper function to get the kth node from the current position
     def getKth(self, curr, k):
         while curr and k > 0:
             curr = curr.next
-            k -=1
+            k -= 1
         return curr
