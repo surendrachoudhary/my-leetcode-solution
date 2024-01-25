804c51dd63bd9a27196dd57800486b4a52cf0649
Surendra Pandar <71205618+surendrachoudhary@users.noreply.github.com>
2024-01-25 08:30:38 +0530

    Time: 48 ms (91.42%) | Memory: 16.7 MB (85.06%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -1,20 +1,32 @@
-from typing import List
-
 class Solution:
-    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
+    def permuteUnique(self, nums):
+        def helper(pos, n, nums, ans):
+            # Base Case executes when we have traversed the entire nums[]
+            if pos >= n:
+                ans.append(nums.copy())
+                return
+
+            # Set ensures we are not taking duplicates and thus not making Duplicate Permutations
+            unique_set = set()
+
+            # The rest logic remains exactly the same
+            for i in range(pos, n):
+                # If we have encountered the element before, we will simply skip the rest of iterations
+                if nums[i] in unique_set:
+                    continue
+
+                # We insert nums[i] so that we don't create Duplicate Permutations
+                unique_set.add(nums[i])
+
+                # We simply use our swapping logic to create Permutations
+                nums[pos], nums[i] = nums[i], nums[pos]
+
+                # Ask recursion to do the rest of the task
+                helper(pos + 1, n, nums, ans)
+
+                # Backtrack and undo the change we have done
+                nums[pos], nums[i] = nums[i], nums[pos]
+
         ans = []
-
-        def backtrack(index, nums):
-            if index == len(nums):
-                if nums not in ans:
-                    ans.append(nums.copy())
-                return 
-            
-            for i in range(index, len(nums)):
-                nums[index], nums[i] = nums[i], nums[index]
-                backtrack(index + 1, nums)
-                nums[index], nums[i] = nums[i], nums[index]
-
-        backtrack(0, nums)
+        helper(0, len(nums), nums, ans)
         return ans
