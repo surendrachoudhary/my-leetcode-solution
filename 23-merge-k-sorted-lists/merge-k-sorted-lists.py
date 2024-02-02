18e5baecf7eb116f3e77fd1437ae766412dcd1ca
Surendra Pandar <71205618+surendrachoudhary@users.noreply.github.com>
2024-02-02 19:24:10 +0530

    Time: 1089 ms (16.73%) | Memory: 19.4 MB (93.59%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -5,22 +5,33 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        node_array = []
-        for i in lists:
+
+        if not lists or len(lists) == 0:
+            return None
+
+        def merge2list(list1, list2):
+            cur = dummy = ListNode()
+            while list1 and list2:               
+                if list1.val < list2.val:
+                    cur.next = list1
+                    list1, cur = list1.next, list1
+                else:
+                    cur.next = list2
+                    list2, cur = list2.next, list2
+                    
+            if list1 or list2:
+                cur.next = list1 if list1 else list2
+                
+            return dummy.next
+
+
+        head = lists[0]
+        for i in lists[1:]:
             if i:
-                while i:
-                    node_array.append(i.val)
-                    i = i.next 
+              head = merge2list(head, i)
             else:
                 continue
+
+        return head
+
         
-        dummy = ListNode(0)
-        tmp = dummy
-
-        node_array.sort()
-
-        for node in node_array:
-            tmp.next = ListNode(node)
-            tmp = tmp.next
-
-        return dummy.next
