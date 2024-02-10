5e0c0fd8326592d7ac52b0b6405b9b07d8a2d4ad
Surendra Pandar <71205618+surendrachoudhary@users.noreply.github.com>
2024-02-10 18:34:11 +0530

    Time: 35 ms (65.02%) | Memory: 16.5 MB (64.33%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.py
+++ 119-pascals-triangle-ii/pascals-triangle-ii.py
@@ -1,25 +1,22 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
         # Initialize an empty list to store the resulting row
-        res = []
+        res = [1]
 
-        # Define a helper function to calculate the value of nCr
-        def nCr(r, c):
-            # Initialize the value to 1
-            value = 1
-
-            # Calculate the value of nCr using the formula (r choose c)
-            for i in range(c):
-                value *= r-i
-                value //= i+1
-
-            # Return the calculated value
-            return value
+        # Calculate the number of elements in the row
+        n = rowIndex + 1
         
-        # Iterate over each index in the row up to the specified rowIndex
-        for i in range(rowIndex+1):
-            # Append the value of nCr to the result list
-            res.append(nCr(rowIndex, i))
+        # Initialize ans to 1
+        ans = 1
+        
+        # Iterate through the row to calculate each element
+        for i in range(1, n):
+            # Update ans using the formula for binomial coefficients (n choose i)
+            ans *= (n - i)
+            ans //= i
+            
+            # Append the calculated value to the result list
+            res.append(ans)
 
         # Return the resulting row
         return res
