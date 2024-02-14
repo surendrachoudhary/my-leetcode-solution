ae38f9ac07a9ab7402c4adcb0160d5cf64e0f025
Surendra Pandar <71205618+surendrachoudhary@users.noreply.github.com>
2024-02-14 18:27:27 +0530

    Time: 497 ms (36.65%) | Memory: 30.2 MB (98.76%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,8 +1,17 @@
+#using the flyod's algorithm cycle detecting in linked list
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        cnt = [0] * (len(nums) + 1)
-        for num in nums:
-            cnt[num] += 1
-            if cnt[num] > 1:
-                return num
-        return len(nums)
+        slow, fast = 0,0
+
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+            if slow == fast:
+                break
+        slow = 0    
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+        
+        return slow
