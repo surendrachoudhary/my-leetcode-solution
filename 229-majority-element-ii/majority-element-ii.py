56221ce0396830381d9ee370a4d1155cca3394a3
Surendra Pandar <71205618+surendrachoudhary@users.noreply.github.com>
2024-02-21 18:47:08 +0530

    Time: 93 ms (87.53%) | Memory: 17.9 MB (83.15%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,17 +1,49 @@
 from collections import Counter
 
 class Solution:
-    def majorityElement(self,arr):
-        # Size of the given array
-        n = len(arr)
+    def majorityElement(self, nums):
+        # Initialize counters for two potential majority elements
+        cnt1, cnt2 = 0, 0 
+        # Initialize placeholders for two potential majority elements
+        elm1, elm2 = "inf", "inf"
+        # Get the length of the input list
+        n = len(nums)
+        # Initialize an empty list to store the majority elements
         ans = []
 
-        # Count the occurrences of each element using Counter
-        counter = Counter(arr)
+        # Find potential majority elements
+        for val in nums:
+            if cnt1 == 0 and val != elm2:
+                elm1 = val
+                cnt1 += 1
+            elif cnt2 == 0 and val != elm1:
+                elm2 = val
+                cnt2 += 1
+            elif val == elm1:
+                cnt1 += 1
+            elif val == elm2:
+                cnt2 += 1 
+            else:
+                cnt1 -= 1
+                cnt2 -= 1
 
-        # Searching for the majority element
-        for num, count in counter.items():
-            if count > (n // 3):
-                ans.append(num)
+        # Reset counters for re-counting
+        cnt1, cnt2 = 0, 0
 
+        # Count occurrences of potential majority elements
+        for val in nums:
+            if val == elm1:
+                cnt1 += 1
+            if val == elm2:
+                cnt2 += 1
+
+        # Calculate the minimum threshold for a majority element
+        mini = n // 3 + 1
+        # Check if potential majority elements meet the threshold and add them to the result list
+        if cnt1 >= mini:
+            ans.append(elm1)
+        if cnt2 >= mini:
+            ans.append(elm2)
+
+        # Return the list of majority elements
         return ans
