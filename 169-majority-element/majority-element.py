f3dba779f45766fe84f414d0bb3481f45dbcd613
Surendra Pandar <71205618+surendrachoudhary@users.noreply.github.com>
2024-02-21 17:48:25 +0530

    Time: 174 ms (9.32%) | Memory: 18 MB (52.04%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,28 +1,14 @@
 class Solution:
-    def majorityElement(self, arr):
-        # Size of the given array
-        n = len(arr)
-        cnt = 0  # Count
-        el = None  # Element
+    def majorityElement(self, nums: List[int]) -> int:
+        count, element = 0, 0
 
-        # Applying the algorithm
-        for i in range(n):
-            if cnt == 0:
-                cnt = 1
-                el = arr[i]
-            elif el == arr[i]:
-                cnt += 1
+        for val in nums:
+            if count == 0 :
+                count += 1
+                element = val
+            elif element == val:
+                count += 1
             else:
-                cnt -= 1
+                count -= 1
 
-        # Checking if the stored element is the majority element
-        cnt1 = 0
-        for i in range(n):
-            if arr[i] == el:
-                cnt1 += 1
-
-        if cnt1 > (n / 2):
-            return el
-        return -1
-
-
+        return element 
