dd1c716d39a7823f2d6d5a0d028d58ca5c66e05d
Surendra Pandar <71205618+surendrapandar@users.noreply.github.com>
2024-03-04 09:44:33 +0530

    Time: 29 ms (89.92%) | Memory: 16.6 MB (80.17%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.py
+++ 225-implement-stack-using-queues/implement-stack-using-queues.py
@@ -8,28 +8,27 @@ class MyStack:
         self.q1.append(x)
 
     def pop(self) -> int:
-        while len(self.q1) > 1:
-            self.q2.append(self.q1.popleft())
-        
-        popped_element = self.q1.popleft()
-        
-        # Swap q1 and q2
-        self.q1, self.q2 = self.q2, self.q1
-        
-        return popped_element
+        if not self.empty():
+            return self.q1.pop()
+        else:
+            return None  
+
 
     def top(self) -> int:
-        while len(self.q1) > 1:
-            self.q2.append(self.q1.popleft())
-        
-        top_element = self.q1[0]
-        
-        self.q2.append(self.q1.popleft())
-        
-        # Swap q1 and q2
-        self.q1, self.q2 = self.q2, self.q1
-        
-        return top_element
+        return self.q1[-1]
 
     def empty(self) -> bool:
-        return len(self.q1) == 0
+        if len(self.q1) > 0:
+            return False 
+        else:
+            return True 
+
+        
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
