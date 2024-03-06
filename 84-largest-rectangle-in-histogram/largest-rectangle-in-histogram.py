11694332ae81842eaa56e73c9f93878364d82af6
Surendra Pandar <71205618+surendrapandar@users.noreply.github.com>
2024-03-06 21:02:47 +0530

    Time: 678 ms (46.85%) | Memory: 30.4 MB (88.43%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -0,0 +1,13 @@
+class Solution:
+    def largestRectangleArea(self, bars: List[int]) -> int:
+        st, res = [], 0
+        for bar in bars + [-1]: # add -1 to have an additional iteration
+            step = 0
+            while st and st[-1][1] >= bar:
+                w, h = st.pop()
+                step += w
+                res = max(res, step * h)
+
+            st.append((step + 1, bar))
+
+        return res
