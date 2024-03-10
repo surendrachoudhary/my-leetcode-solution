93619d8ed8fc0c99c56de4cdf2864d7f10a92440
Surendra Pandar <71205618+surendrapandar@users.noreply.github.com>
2024-03-10 11:36:15 +0530

    Time: 45 ms (26.48%) | Memory: 16.6 MB (78.07%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.py
+++ 686-repeated-string-match/repeated-string-match.py
@@ -1,17 +1,19 @@
 class Solution:
     def repeatedStringMatch(self, a: str, b: str) -> int:
-        # Calculate the minimum number of times to repeat a
-        min_repeats = -(-len(b) // len(a))  # Equivalent to ceil(len(b) / len(a))
 
-        # Concatenate a repeated min_repeats times and check if b is a substring
-        repeated_a = a * min_repeats
-        if b in repeated_a:
-            return min_repeats
+        min_repeat = -(-len(b)//len(a))
 
-        # If b is not a substring of a repeated min_repeats times, check if it is a substring after repeating one more time
-        repeated_a += a
-        if b in repeated_a:
-            return min_repeats + 1
+        string = a * min_repeat
 
-        # If b is still not a substring after repeating one more time, return -1
+        
+        if b in string:
+            return min_repeat
+        
+        string += a
+
+        if b in string:
+            return min_repeat + 1
+
         return -1
+
+        
