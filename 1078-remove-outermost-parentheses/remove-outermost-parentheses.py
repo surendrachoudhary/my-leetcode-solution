69553fc06d6a1b268ac3596d33dfd97fb770d2ef
Surendra Pandar <71205618+surendrapandar@users.noreply.github.com>
2024-03-10 16:06:47 +0530

    Time: 40 ms (50.42%) | Memory: 16.7 MB (70.87%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.py
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.py
@@ -1,23 +1,16 @@
 class Solution:
     def removeOuterParentheses(self, s: str) -> str:
-        open_bracket = 0
         stack = []
         ans = ""
 
-        for i in s:
-            if i == "(":
-                stack.append(i)
-                open_bracket += 1
-                if open_bracket > 1:
-                    ans = ans + "("
+        for char in s:
+            if char == "(":
+                if stack:  # If stack is not empty, add the current character to ans
+                    ans += char
+                stack.append(char)
             else:
-                if open_bracket < 2:
-                    stack.pop()
-                    open_bracket -= 1
-                else:
-                    ans = ans + ")"
-                    stack.pop()
-                    open_bracket -= 1
+                stack.pop()  # Remove the corresponding "(" from stack
+                if stack:  # If stack is not empty, add the current character to ans
+                    ans += char
         
-
         return ans
