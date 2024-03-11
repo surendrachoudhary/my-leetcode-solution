cf968a7e6ca115be788279af41de047006df68e6
Surendra Pandar <71205618+surendrapandar@users.noreply.github.com>
2024-03-11 18:49:34 +0530

    Time: 86 ms (75.06%) | Memory: 21.4 MB (42.22%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,27 @@
+from collections import Counter
+import heapq
+from typing import List
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # Initialize an empty list to store the top k frequent elements
+        ans = []
+        # Count the frequency of each element in the input list
+        counter = Counter(nums)
+        # Initialize a heap to store tuples of frequency and element
+        heap = []
+
+        # Iterate through the items in the counter
+        for num, freq in counter.items():
+            # Push tuples containing frequency and element into the heap
+            heapq.heappush(heap, (-freq, num))
+        
+        # Pop elements from the heap until it is empty
+        while heap:
+            # Pop the tuple with the highest frequency from the heap
+            freq, num = heapq.heappop(heap)
+            # Append the element to the answer list
+            ans.append(num)
+
+        # Return the top k frequent elements
+        return ans[:k]
