5d264823a115d3ef6e57a95aa0f611cd161c4a76
Surendra Pandar <71205618+surendrapandar@users.noreply.github.com>
2024-06-28 22:44:21 +0530

    Time: 146 ms (52.73%) | Memory: 17 MB (58.34%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.py
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -1,34 +1,26 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
         n = len(graph)
-        color_list = [0] * (n)
-        visited_set = set()
-        
-        def bfs(i):
-            q = deque()
-            q.append(i)
-            visited_set.add(i)
+        color_list = [0] * n  # 0: uncolored, 1: color1, -1: color2
 
-            color_list[i] = -1
+        def bfs(start):
+            q = deque([start])
+            color_list[start] = 1  # Start coloring with 1
 
             while q:
                 node = q.popleft()
+                for neighbor in graph[node]:
+                    if color_list[neighbor] == 0:  # If not colored, color with opposite color
+                        color_list[neighbor] = -color_list[node]
+                        q.append(neighbor)
+                    elif color_list[neighbor] == color_list[node]:  # If same color as current node, not bipartite
+                        return False
+            return True
 
-                for nei in graph[node]:
-                    if nei not in visited_set:
-                        color_list[nei] = -color_list[node] 
-                        q.append(nei)
-                        visited_set.add(nei)
-                    elif color_list[nei] == color_list[node]:
-                        return False 
-            return True 
-
+        # Need to check each component of the graph
         for i in range(n):
-            if i not in visited_set:
+            if color_list[i] == 0:  # If the node is not yet colored, start a BFS
                 if not bfs(i):
-                    return False 
-       
-        # print(color_list)        
-        return True 
+                    return False
 
-     
+        return True
