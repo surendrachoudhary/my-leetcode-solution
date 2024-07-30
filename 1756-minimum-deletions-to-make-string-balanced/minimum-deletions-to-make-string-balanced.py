f8482953359863e6b1855d9ddb08192b019d20d9
Surendra Pandar <71205618+surendrapandar@users.noreply.github.com>
2024-07-30 13:22:41 +0530

    Time: 165 ms (92.33%) | Memory: 17.7 MB (79.00%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.py
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.py
@@ -1,21 +1,33 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
-        a_count_right = 0
+        # a_count_right = 0
 
-        for i in s:
-            if i == "a":
-                a_count_right += 1
+        # for i in s:
+        #     if i == "a":
+        #         a_count_right += 1
 
-        b_count_left = 0
-        res = len(s)
+        # b_count_left = 0
+        # res = len(s)
 
-        for i in s:
-            if i == "a":
-                a_count_right -= 1
+        # for i in s:
+        #     if i == "a":
+        #         a_count_right -= 1
             
-            res = min(res, a_count_right + b_count_left)
+        #     res = min(res, a_count_right + b_count_left)
 
+        #     if i == "b":
+        #         b_count_left += 1
+
+        # return res 
+
+
+        count = 0
+        stack = []
+
+        for i in s:
             if i == "b":
-                b_count_left += 1
-
-        return res 
+                stack.append(i)
+            elif stack:
+                stack.pop()
+                count += 1
+        return count 
