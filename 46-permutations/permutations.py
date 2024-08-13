94014f24e55885d18d4dbdd47ddd9eca5058069c
Surendra Pandar <71205618+surendrapandar@users.noreply.github.com>
2024-08-13 15:14:40 +0530

    Time: 34 ms (91.06%) | Memory: 16.7 MB (27.62%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,30 +1,19 @@
-from typing import List
-
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        # Initialize an empty list to store the permutations
-        ans = []
-
-        def backtrack(index, nums):
-            # If the current index reaches the end of the list, a permutation is complete
+        def helper(index, nums):
             if index == len(nums):
-                # Append a copy of the current nums list to the ans list
-                ans.append(nums.copy())
-                return 
-            
-            # Iterate through elements starting from the current index
-            for i in range(index, len(nums)):
-                # Swap the elements at the current index (by itself)
-                nums[index], nums[i] = nums[i], nums[index]
-                
-                # Recursively call the backtrack function for the next index
-                backtrack(index + 1, nums)
-                
-                # Undo the swap to backtrack and try other possibilities
-                nums[index], nums[i] = nums[i], nums[index]
+                return  [[]]
 
-        # Start the backtrack process from the beginning (index 0)
-        backtrack(0, nums)
+            result = []
 
-        # Return the list of permutations
-        return ans
+            listing = helper( index +1, nums)
+
+            for p in listing:
+                for j in range(len(p) + 1):
+                    pCopy = p.copy()
+                    pCopy.insert(j, nums[index])
+                    result.append(pCopy)
+
+            return result     
+
+        return helper(0, nums)
