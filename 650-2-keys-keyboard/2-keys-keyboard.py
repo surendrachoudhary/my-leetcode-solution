ff89c6cccd7e4a9e25fc41bf59bf0e4154de6e75
Surendra Pandar <71205618+surendrapandar@users.noreply.github.com>
2024-08-21 13:33:38 +0530

    Time: 130 ms (40.27%) | Memory: 27.8 MB (13.08%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.py
+++ 650-2-keys-keyboard/2-keys-keyboard.py
@@ -2,18 +2,22 @@ class Solution:
     def minSteps(self, n: int) -> int:
         if n == 1:
             return 0
+        
+        cache = {}
         def helper(count, paste):
             if count == n:
                 return 0
             if count > n:
                 return 1000
-            
+            if (count, paste) in cache:
+                return cache[(count, paste)]
             #Paste 
             res1 = 1 + helper(count + paste, paste)
 
             # copy and paste 
             res2 = 2 + helper(count + count, count)
 
-            return min(res1, res2)
+            cache[(count,paste)] = min(res1, res2)
+            return cache[(count, paste)]
             
         return 1 + helper(1, 1)
